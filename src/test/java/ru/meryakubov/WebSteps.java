7d1ed2895877c0ea3756d4e5b96a6c790da4143f
ameryakubov <alex_meryakubov@mail.ru>
2023-08-24 22:13:46 +0300

    initial commit

--- src/test/java/ru/meryakubov/WebSteps.java
+++ src/test/java/ru/meryakubov/WebSteps.java
@@ -1,8 +1,12 @@
 package ru.meryakubov;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.WebDriverRunner;
+import io.qameta.allure.Attachment;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
@@ -36,5 +40,9 @@ public class WebSteps {
     @Step("Проверяем наличие Issue с номером {issue}")
     public void shouldIssueWithNumber(int issue) {
         $(withText("#" + issue)).should(Condition.exist);
+    }
+    @Attachment(value = "Screenshot", type = "image/png",fileExtension = "png")
+    public byte[] takeScreenshot() {
+        return ((TakesScreenshot)WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES);
     }
 }
